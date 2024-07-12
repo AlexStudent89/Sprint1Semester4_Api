867b73f8188ccd3fc6e7a11b3456c02e8801c0ea
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-07-12 11:53:43 -0230

    update Airport & city controller files

--- src/main/java/com/keyin/Sprint1Semester4_Api/controller/AirportController.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/controller/AirportController.java
@@ -1,4 +1,26 @@
 package com.keyin.Sprint1Semester4_Api.controller;
 
+import com.keyin.Sprint1Semester4_Api.model.Airport;
+import com.keyin.Sprint1Semester4_Api.service.AirportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/airport")
 public class AirportController {
+
+    @Autowired
+    private AirportService airportService;
+
+    @GetMapping
+    public List<Airport> getAllAirports() {
+        return airportService.getAllAirports();
+    }
+
+    @PostMapping
+    public void addAirport(@RequestBody Airport airport) {
+        airportService.addAirport(airport);
+    }
 }
--- src/main/java/com/keyin/Sprint1Semester4_Api/controller/CityController.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/controller/CityController.java
@@ -1,4 +1,26 @@
 package com.keyin.Sprint1Semester4_Api.controller;
 
+import com.keyin.Sprint1Semester4_Api.model.City;
+import com.keyin.Sprint1Semester4_Api.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/city")
 public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public List<City> getAllCities() {
+        return cityService.getAllCities();
+    }
+
+    @PostMapping
+    public void addCity(@RequestBody City city) {
+        cityService.addCity(city);
+    }
 }
