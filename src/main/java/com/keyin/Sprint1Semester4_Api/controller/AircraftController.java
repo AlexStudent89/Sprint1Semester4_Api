b9b4c9ae19e441f44088a4c5be30de6634eea492
MohamedMaghrebi <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-07-12 11:43:19 -0230

    Update and fix

--- src/main/java/com/keyin/Sprint1Semester4_Api/controller/AircraftController.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/controller/AircraftController.java
@@ -1,5 +1,27 @@
 //AircraftController
 package com.keyin.Sprint1Semester4_Api.controller;
 
+import com.keyin.Sprint1Semester4_Api.model.Aircraft;
+import com.keyin.Sprint1Semester4_Api.service.AircraftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aircraft")
 public class AircraftController {
+
+    @Autowired
+    private AircraftService aircraftService;
+
+    @GetMapping
+    public List<Aircraft> getAllAircraft() {
+        return aircraftService.getAllAircraft();
+    }
+
+    @PostMapping
+    public void addAircraft(@RequestBody Aircraft aircraft) {
+        aircraftService.addAircraft(aircraft);
+    }
 }
