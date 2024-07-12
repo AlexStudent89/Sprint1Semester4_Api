3671482718c6f16c6656dedb4fdda158bc715a50
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-07-11 22:34:08 -0230

    Update Aircraft java file

--- src/main/java/com/keyin/Sprint1Semester4_Api/model/Aircraft.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/model/Aircraft.java
@@ -1,4 +1,29 @@
 package com.keyin.Sprint1Semester4_Api.model;
 
 public class Aircraft {
+    private Long id;
+    private String type;
+    private String airlineName;
+    private int numberOfPassengers;
+
+    public Aircraft() {}
+
+    public Aircraft(Long id, String type, String airlineName, int numberOfPassengers) {
+        this.id = id;
+        this.type = type;
+        this.airlineName = airlineName;
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getAirlineName() { return airlineName; }
+    public void setAirlineName(String airlineName) { this.airlineName = airlineName; }
+
+    public int getNumberOfPassengers() { return numberOfPassengers; }
+    public void setNumberOfPassengers(int numberOfPassengers) { this.numberOfPassengers = numberOfPassengers; }
 }
