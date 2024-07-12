200a5cd1d239f44fc8329570b4c4e37bb91fd863
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-07-11 23:01:26 -0230

    Updating files
    City.java

--- src/main/java/com/keyin/Sprint1Semester4_Api/model/City.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/model/City.java
@@ -1,4 +1,29 @@
 package com.keyin.Sprint1Semester4_Api.model;
 
 public class City {
+    private Long id;
+    private String name;
+    private String state;
+    private int population;
+
+    public City() {}
+
+    public City(Long id, String name, String state, int population) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+        this.population = population;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
+    public int getPopulation() { return population; }
+    public void setPopulation(int population) { this.population = population; }
 }
