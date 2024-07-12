7bd71df50564ffc1cfa6af5f68a9fb4b596ffd87
MohamedMaghrebi <138633077+MohamedMaghrebi@users.noreply.github.com>
2024-07-12 12:16:50 -0230

    update passenger service

--- src/main/java/com/keyin/Sprint1Semester4_Api/service/PassengerService.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/service/PassengerService.java
@@ -1,4 +1,19 @@
 package com.keyin.Sprint1Semester4_Api.service;
 
+import com.keyin.Sprint1Semester4_Api.model.Passenger;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PassengerService {
+    private List<Passenger> passengerList = new ArrayList<>();
+
+    public List<Passenger> getAllPassengers() {
+        return passengerList;
+    }
+
+    public void addPassenger(Passenger passenger) {
+        passengerList.add(passenger);
+    }
 }
