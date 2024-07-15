35e03f8a2de53c5b495b8989a44cacef54c4695a
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-07-15 13:07:09 -0230

    Update passenger service file

--- src/main/java/com/keyin/Sprint1Semester4_Api/service/PassengerService.java
+++ src/main/java/com/keyin/Sprint1Semester4_Api/service/PassengerService.java
@@ -1,6 +1,7 @@
 package com.keyin.Sprint1Semester4_Api.service;
 
 import com.keyin.Sprint1Semester4_Api.model.Passenger;
+import com.keyin.Sprint1Semester4_Api.model.Aircraft;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,43 @@ public class PassengerService {
     public void addPassenger(Passenger passenger) {
         passengerList.add(passenger);
     }
+      //To retrieve a passenger by index
+//    public Passenger getPassenger(int index) {
+//        if (index >= 0 && index < passengers.size()) {
+//            return passengers.get(index);
+//        }
+//        return null;
+//    }
 
+    // To create a new passenger
+//    public Passenger createPassenger(Passenger newPassenger) {
+//        passengers.add(newPassenger);
+//        return newPassenger;
+//    }
+
+    // To Update a passenger
+//    public Passenger updatePassenger(int index, Passenger updatedPassenger) {
+//        if (index >= 0 && index < passengers.size()) {
+//            passengers.set(index, updatedPassenger);
+//            return updatedPassenger;
+//        }
+//        return null;
+//    }
+
+    // To get a plane for a passenger
+//    public List<Aircraft> getAircraftForPassenger(int index) {
+//        if (index >= 0 && index < passengers.size()) {
+//            return passengers.get(index).getAircraftList();
+//        }
+//        throw new RuntimeException("Passenger not found with index: " + index);
+//    }
+
+    // To remove a passenger
+//    public void deletePassenger(int index) {
+//        if (index >= 0 && index < passengers.size()) {
+//            passengers.remove(index);
+//        }
+//    }
 
 
 }
